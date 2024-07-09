0636142c2684844d9df7113f605657e65c67391d
Ashkan <AshkanHagh01@gmail.com>
2024-07-09 09:57:20 +0330

    Fix redis connection

--- db/redis.ts
+++ db/redis.ts
@@ -1,13 +1,3 @@
 import { Redis } from 'ioredis';
 
-const redisClient = () => {
-
-    if(process.env.Redis_Url) {
-        console.log('Redis connected');
-        return process.env.Redis_Url;
-    }
-
-    throw new Error('Redis connection failed');
-}
-
-export const redis = new Redis(redisClient());
+export const redis = new Redis(process.env.Redis_Url as string);
