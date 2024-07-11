d06990fa662ee42f8cb0b94367493b98db0a6f91
Ashkan <AshkanHagh01@gmail.com>
2024-07-11 12:26:44 +0330

    Fix cors error

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json({limit : '10mb'}));
 app.use(express.urlencoded({extended : true}));
 app.use(cookieParser());
-app.use(cors({origin : process.env.ORIGIN, methods : ['POST', 'GET'], allowedHeaders : '*'}));
+app.use(cors());
 app.use(helmet());
 app.use(helmet({crossOriginResourcePolicy : {policy : 'cross-origin'}}));
 
