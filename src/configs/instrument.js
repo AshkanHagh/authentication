b0ea06b7675a8930a498f346dd95764a99042875
Ashkan <AshkanHagh01@gmail.com>
2024-08-17 09:34:05 +0330

    fix(folders): fix folder structure

--- src/configs/instrument.js
+++
@@ -1,7 +0,0 @@
-import * as Sentry from '@sentry/bun';
-
-Sentry.init({
-    dsn: process.env.SENTRY_KEY,
-    integrations: [],
-    tracesSampleRate: 1.0,
-});
--- src/server.ts
+++
@@ -1,6 +0,0 @@
-import app from './app';
-
-Bun.serve({
-    port : process.env.PORT || 6150,
-    fetch : app.fetch
-})
